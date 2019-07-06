03c08dfbf6ff34e05c573355fac491b73e8e3098
Samuel Rodriguez <samuelyret@gmail.com>
2019-07-06 16:34:35 +0200

    Simplified and expanded regex subtitution for the non-alphanumerical repeated characters.

--- label_extract/utils/text.py
+++ label_extract/utils/text.py
@@ -8,17 +8,15 @@ class Text:
     def clean(self, text):
         patterns = [
             (r'^\s?\d+\.[\s\t]?', ''),  # Numbered bullet points at start of paragraph
-            (r' *\--+', ' '),           # Extra long number of dashes ------
-            (r' *\__+', ' '),           # Extra long number of underscores ____
+            (r' ?( ?\W)\1+', r' \1 '),  # Reduce repeated non alphanumeric characters, -------
             (r'\s\s+', ' '),            # Two or more spaces
             (r'\t', ''),                # Tabulations
             (r'\xa0', ' '),             # Non-breaking space \xa0
-            (r'(\r\n|\r|\n)', ' '),     # Line breaks. Check this one as there might be some paragraphs that need to be separated
+            (r'(\r\n|\r|\n)', ' '),     # Line breaks
             (r'\•', ''),                # Bullet points
             (r'^\s?\(\w+\)\s*', ''),
             (r'(\|?\s\|)+', ' '),       # Vertical bars,  such as '| |goals | |'
             (r'^\W*', ''),              # Any non-alphanumerical character at the start of string
-            (r'\s*\.\.+', ''),          # Extra long number of periods .....
             (r'Œ', '-')                 # Replace corrupted dash
         ]
         # Run regex twice so that is not affected by order
